e8e9a34bc2198f5edc51d1c9b5c566645bb62111
rubyace71697 <kukreja34@howmail.com>
2015-06-09 19:13:42 +0530

    Added lice icons

--- espn_indicator.py
+++ espn_indicator.py
@@ -7,7 +7,7 @@ Created on 06-Jun-2015
 from gi.repository import Gtk , GObject
 from gi.repository import AppIndicator3 as appindicator
 from espn_scrap import espn_scrap
-
+from about import About
 import urllib2
 from bs4 import BeautifulSoup
 import thread
@@ -15,28 +15,32 @@ import time
 import signal
 import json
 import requests
+import os.path
+import sys
 
 REFRESH_TIMEOUT = 2 # second(s)
 PING_FREQUENCY = 1 # seconds
 APP_ID = "new-espn-indicator"
-
+ICON_PATH = "/home/nishant/workspace/cricket-score-applet/screenshots/1.png"
 
 class espn_ind:
     def __init__(self):
         """
         Initialize appindicator and other menus
         """
+        
 
         # initialize the appindicator
-        self.indicator = appindicator.Indicator.new(APP_ID,"indicator-messages",appindicator.IndicatorCategory.APPLICATION_STATUS)
+        self.indicator = appindicator.Indicator.new(APP_ID , ICON_PATH , appindicator.IndicatorCategory.APPLICATION_STATUS)
         self.indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
         self.scrap = espn_scrap()
 
         # create the menu and submenu
+        self.label_disp_index = 0
         self.menu_setup()
         self.indicator.set_menu(self.menu)
 
-        self.label_disp_index = 0
+        
 
     def menu_setup(self):
         """
@@ -47,49 +51,66 @@ class espn_ind:
 
 
         self.match = self.scrap.get_matches_summary()
+        """
+        for x in self.match:
+            print x
+        """
         self.match_item_menu = []
 
         i = 0
         for match_info in self.match:
             self.match_item = {}
 
-            # TODO: use consistent naming scheme
-            # i.e. prepend 'gtk' or 'menuitem' before gtk objects' names
-            self.match_item['label'] = Gtk.MenuItem(match_info['match_score_summary'])
-            self.match_item['label_text'] = match_info['match_score_summary']
-            self.match_item['url'] = match_info['match_url']
+            
+            
 
-            self.match_item['submenu'] = Gtk.Menu()
-            self.match_item['show'] = Gtk.MenuItem("\nSet as Label")
+            self.match_item = {
+
+                                'label' : Gtk.ImageMenuItem(Gtk.STOCK_NEW, match_info['match_score_summary']),
+                                'label_text' : match_info['match_score_summary'],
+                                'url' : match_info['match_url'],
+                                'submenu' : Gtk.Menu(),
+                                'show' : Gtk.MenuItem("Set as Label"),
+                                'description' : "Loading",
+                                'match_ball' : "Loading",
+                                'gtk_description' : Gtk.MenuItem("Loading"),
+                                'scorecard' : Gtk.MenuItem("Loading"),
+                                'scorecard_text' : "Loading" ,
+            }
+
             self.match_item['show'].connect("activate",self.show_clicked,i)
-
-            self.match_item['scorecard'] = Gtk.MenuItem("Loading")
             self.match_item['scorecard'].set_sensitive(False)
-            self.match_item['scorecard_text'] = "Loading"
-
-            # attach menuitems and submenu togther as one unit
-            self.match_item['submenu'].append(self.match_item['show'])
-            self.match_item['submenu'].append(self.match_item['scorecard'])
-
-            self.match_item['label'].set_submenu(self.match_item['submenu'])
-
             self.menu.append(self.match_item['label'])
+            self.match_item['submenu'].append(self.match_item['show'])
+            self.match_item['submenu'].append(self.match_item['gtk_description'])
+            self.match_item['submenu'].append(self.match_item['scorecard'])
+            self.match_item['label'].set_submenu(self.match_item['submenu'])
+            img = Gtk.Image()
+            img.set_from_file("/home/nishant/workspace/cricket-score-applet/screenshots/six.png")
+            self.match_item['label'].set_image(img)
+            self.match_item['label'].set_always_show_image(True)
 
             # make a list to keep them all togther
             self.match_item_menu.append(self.match_item)
 
             self.match_item_menu[i]['label'].show()
+            #self.match_item_menu[i]['gtk_description'].show()
             self.match_item_menu[i]['show'].show()
             self.match_item_menu[i]['scorecard'].show()
 
             i += 1
 
-        self.menu.show_all()
+        #self.menu.show_all()
 
         # you have to attatch the window in future
         self.preferences_item = Gtk.MenuItem("Preferences <beware its not working>")
         self.menu.append(self.preferences_item)
         self.preferences_item.show()
+
+        self.about_item = Gtk.MenuItem("About")
+        self.menu.append(self.about_item)
+        self.about_item.show()
+        self.about_item.connect("activate",self.about)
 
         # we need a way to quit if the indicator is irritating ;)
         self.quit_item = Gtk.MenuItem("Quit")
@@ -108,6 +129,9 @@ class espn_ind:
     def quit(self, widget):
 
         Gtk.main_quit()
+
+    def about(self,widget):
+    	About().display()
 
     def show_clicked(self,widget, i):
         self.label_disp_index = i
@@ -137,6 +161,15 @@ class espn_ind:
             TODO: make a new function for getting the data
         """
         self.match = self.scrap.get_matches_summary()
+        """
+        print
+        print "in espn_indicator.py"
+        print "in update_labels"
+        print "json data"
+        
+        for x in self.match:
+            print x
+        """
         j = 0
 
         for match_info in self.match:
@@ -147,20 +180,49 @@ class espn_ind:
 
             # update the indicaror status
             if j == self.label_disp_index :
-                GObject.idle_add(self.set_indicator_status)
+                GObject.idle_add(self.set_indicator_status , self.match_item_menu[j]['match_ball'])
 
             j += 1
+
+        """
+        print "updated menu"
+        for x in self.match_item_menu:
+            print x
+        """
+
+    """
+        handlers
+    """
 
     def set_menu_item(self,index,label_text):
         self.match_item_menu[index]['label'].set_label(label_text)
 
-    def set_indicator_status(self):
+    def set_indicator_status(self, icon_name):
         self.indicator.set_label(self.match_item_menu[self.label_disp_index]["label_text"],"")
+        print self.match_item_menu[self.label_disp_index]['url']
+        
+        self.indicator.set_icon("/home/nishant/workspace/cricket-score-applet/screenshots/"+ icon_name +".png")
 
     def set_submenu_item(self, index , scorecard_text ):
         self.match_item_menu[index]['scorecard'].set_label( scorecard_text)
 
+    def set_description(self,index, description_text):
+        self.match_item_menu[index]['gtk_description'].set_label(description_text)
+
+    def update_icon(self,index, icon_name):
+        print "here"
+        img = Gtk.Image()
+        img.set_from_file("/home/nishant/workspace/cricket-score-applet/screenshots/"+ icon_name +".png")
+        self.match_item_menu[index]['label'].set_image(img)
+
+
+
     def  check_submenu(self):
+        """
+        print
+        print "in espn_indicator.py"
+        print "in check_submenu"
+        """
         match_info = {}
         j = 0
 
@@ -169,8 +231,23 @@ class espn_ind:
 
             match_info = self.scrap.check_match_summary(url,j)
             self.match_item_menu[j]['scorecard_text'] = match_info['match_scorecard_summary']
+            self.match_item_menu[j]['match_scorecard_summary'] = match_info['match_scorecard_summary']
+            self.match_item_menu[j]['description'] = match_info['description']
+            if( match_info['match_ball'] and match_info['match_ball'] == '&bull;'):
+                match_info['match_ball'] = '0'
+            self.match_item_menu[j]['match_ball'] = match_info['match_ball']
+            print match_info['match_ball']
+            GObject.idle_add(self.update_icon, j , str(match_info['match_ball']))
             GObject.idle_add(self.set_submenu_item , j ,match_info['match_scorecard_summary'])
+            GObject.idle_add(self.set_description , j ,match_info['description'])
+            
             j += 1
+
+        """
+        for x in self.match_item_menu:
+            print x
+        """
+
 
 
 if __name__ == "__main__":
--- espn_scrap.py
+++ espn_scrap.py
@@ -13,6 +13,8 @@ class espn_scrap:
         # for maintaing list of matches
         match_info = {}
         """
+            TODO: Update the new fields
+
             "match_info" will contain following properties:
 
             match_score_summary {
@@ -32,6 +34,8 @@ class espn_scrap:
         """
         self.dummy_match_info = {
                 'match_score_summary': 'No data available: check networking settings',
+                'match_description': 'No description available',
+                'match_ball' : "No description available",
                 'match_scorecard_summary': 'No summary available',
                 'match_url': 'http://127.0.0.1',
                 }
@@ -49,6 +53,8 @@ class espn_scrap:
         """
         Data fields returned by JSON:
             live_match,     // boolean flag
+            description,    //info about match
+            match_ball,
             match_clock,    // time duration from start
             url,
             result,
@@ -72,20 +78,26 @@ class espn_scrap:
             # TODO: consider using match_clock if startstring is not available
             summary_text = "{team1}{team1score} vs {team2}{team2score} {startstring}".format(
                     team1       = all_matches[i]['team1_name'].strip().replace('&nbsp;', " "),
-                    team1score  = (" - " + all_matches[i]['team1_score'].strip().replace('&nbsp;', " ")) if all_matches[i]['team1_score'].strip() else "",
+                    team1score  = (" - " + all_matches[i]['team1_score'].strip().replace('&nbsp;', " ").replace('&amp;', '&')) if all_matches[i]['team1_score'].strip() else "",
                     team2       = all_matches[i]['team2_name'].strip().replace('&nbsp;', " "),
-                    team2score  = (" - " + all_matches[i]['team2_score'].strip().replace('&nbsp;', " ")) if all_matches[i]['team2_score'].strip() else "",
+                    team2score  = (" - " + all_matches[i]['team2_score'].strip().replace('&nbsp;', " ").replace('&amp;', '&')) if all_matches[i]['team2_score'].strip() else "",
                     startstring = (" - " + str(all_matches[i]['start_string']).strip().replace('&nbsp;', " ")) if 'start_string' in all_matches[i] else ""
                     )
 
+            
             match_info = {
                     'match_url':               all_matches[i]['url'],
+                    
                     'match_score_summary':     summary_text,
                     'match_scorecard_summary': "Loading"
                     }
 
             self.match.append(match_info)
-
+            """
+            print "in get matches summary"
+            for s in self.match:
+                print s
+            """
         return self.match
 
     def check_match_summary(self, url, count):
@@ -131,7 +143,12 @@ class espn_scrap:
                     match_summary += str(y) + ": " + str(x[y])
 
         """
+        self.match[count]['match_ball'] = ""
+        if(json_data['live']['recent_overs']):
+            self.match[count]['match_ball'] = json_data['live']['recent_overs'][-1][-1]['ball']
 
+        #print "in espn_scrap : " + str(count) + " : " +  json_data['live']['recent_overs'][-1][-1]['ball']
+        self.match[count]['description'] = json_data['description']
         match_summary += "\n" + str(json_data['live']['status'])
         match_summary += "\n" + str(json_data['live']['break']) + "\n"
 
@@ -177,5 +194,7 @@ class espn_scrap:
                 match_summary += "\n"
 
         self.match[count]['match_scorecard_summary'] = match_summary
+        #print "in espn_scrap"
+        #print self.match[count]['description']
 
         return self.match[count]
